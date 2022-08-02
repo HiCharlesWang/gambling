af3387dfb1f79cd40971619f9754e7b1af6bdd26
HiCharlesWang <yxbscpioneer@gmail.com>
2022-08-02 13:54:17 +0200

    formatted, implemented increment, gameAddress immutable

--- contracts/game.sol
+++ contracts/game.sol
@@ -1,33 +1,32 @@
-pragma solidity >= 0.8.0;
+pragma solidity >=0.8.0;
 
-
-
-interface IVAULT{
+interface IVAULT {
     function takeETH(uint256 _amount) external;
-    function depositETH() payable external;
+
+    function depositETH() external payable;
+
     function reserveETH(uint256 _amount) external;
 }
 
 contract gameContract {
     address public vault;
+
     function setVault(address _addr) external {
         vault = _addr;
     }
-    function sendETH() payable external{
+
+    function sendETH() external payable {
         IVAULT(vault).depositETH{value: msg.value}();
     }
 
-
     function takeETH(uint256 _amount) external {
         IVAULT(vault).takeETH(_amount);
-    //  payable(_winner).transfer(_amount)
+        //  payable(_winner).transfer(_amount)
     }
 
-    function reserveETH(uint256 _amount) external{
+    function reserveETH(uint256 _amount) external {
         IVAULT(vault).reserveETH(_amount);
     }
 
-    receive() external payable {
-
-    }
+    receive() external payable {}
 }
